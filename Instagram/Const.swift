2decfcb307e182af31e58f26c2c01a4a1ecd7a6b
Sora-bs <aozoma@icloud.com>
2019-07-20 10:51:50 +0900

    commit

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by sora on 2019/07/20.
+//  Copyright © 2019 sora. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
